509655bc809f799c682483ecc55db1365e98b867
Nader <nader_84@hotmail.fr>
2021-11-15 15:18:19 +0100

    Q2 : generate and load the best model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import datetime
 
+import keras
 from keras import Sequential
 from keras.callbacks import TensorBoard
 from keras.layers import Dense, BatchNormalization
@@ -10,11 +11,7 @@ mnist = tf.keras.datasets.mnist
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
-
-
-
-
-#model = create_model()
+# Q1 et Q2 create model, create callbacks, load the best model
 model = Sequential()
 model.add(BatchNormalization(input_shape=(28*28,)))  # nb d'entrées égale à la dimension des images, on remplace le 400 par 28*28
 model.add(Dense(200, activation='sigmoid'))
@@ -27,6 +24,14 @@ model.compile(optimizer='adam',
 
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath="model/best_model",
+    monitor='val_categorical_accuracy',
+    mode='max',
+    verbose=1,
+    save_best_only=True)
+
+
 
 
 x_train = x_train.reshape(60000, 28*28)        # nb_dimension est 28*28 = 784 puisque ce sont des images d'une dimension de 28*28 pixels
@@ -39,23 +44,13 @@ model.fit(x=x_train,
           y=y_train,
           epochs=20,
           validation_data=(x_test, y_test),
-          callbacks=[tensorboard_callback])
+          callbacks=[tensorboard_callback, model_checkpoint_callback])
 
 
 
-EPOCHS = 10
-checkpoint_filepath = '/tmp/checkpoint'
-model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath,
-    save_weights_only=True,
-    monitor='val_accuracy',
-    mode='max',
-    save_best_only=True)
 
-# Model weights are saved at the end of every epoch, if it's the best seen
-# so far.
-model.fit(epochs=EPOCHS, callbacks=[model_checkpoint_callback])
+best_model = keras.models.load_model("model/best_model")
 
-# The model weights (that are considered the best) are loaded into the model.
-modelBest = model.load_weights(checkpoint_filepath)
-
+score = best_model.evaluate(x_test, y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
