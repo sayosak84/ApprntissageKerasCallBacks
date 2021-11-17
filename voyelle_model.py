3a4f37ff65a8b6e1fdc2cf9f4b4b06aa4f6d9c49
Nader <nader_84@hotmail.fr>
2021-11-17 17:15:33 +0100

    Q3 : execute model and save the best model using binary_accuracy

--- voyelle_model.py
+++ voyelle_model.py
@@ -8,6 +8,7 @@ from keras.callbacks import TensorBoard
 from keras.layers import Dense, BatchNormalization
 from keras.utils.np_utils import to_categorical
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
 
 
@@ -30,8 +31,8 @@ dataset = pd.read_csv('voyelles.txt', sep='\t', usecols=['voyelle', 'F1', 'F2', 
 dataset = dataset.replace('--undefined--',np.nan)                    # replace the unkown values by np.nan
 print(dataset['F4'][66672])                                          # check the value
 simple_impute = SimpleImputer(strategy='median')                     # Initialise simpleImputer with a median strategy
-X = simple_impute.fit_transform(dataset[dataset.columns[1:]])  # replace the np.nan values by the median => X values
-Y = dataset[dataset.columns[0]]                                # get the Y values => voyelles dataset
+X = simple_impute.fit_transform(dataset[dataset.columns[1:]])        # replace the np.nan values by the median => X values
+Y = dataset[dataset.columns[0]]                                      # get the Y values => voyelles dataset
 print(unique(Y))
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, train_size=0.80)   # get train dataset 80%
 x_test, x_valid, y_test, y_valid = train_test_split(x_test, y_test, test_size=0.50, train_size=0.50)  # get test and validation datasets 50% of 20%
@@ -41,8 +42,15 @@ x_test, x_valid, y_test, y_valid = train_test_split(x_test, y_test, test_size=0.
 
 
 # todo : change letter to int or float
+
+label_encoder = preprocessing.LabelEncoder()
+y_train = label_encoder.fit_transform(y_train)
 y_train = to_categorical(y_train, 10)
+
+y_test = label_encoder.fit_transform(y_test)
 y_test = to_categorical(y_test, 10)
+
+y_valid = label_encoder.fit_transform(y_valid)
 y_valid = to_categorical(y_valid, 10)
 
 # create the model
@@ -52,13 +60,14 @@ model.add(Dense(200, activation='sigmoid'))
 model.add(Dense(200, activation='sigmoid'))
 model.add(Dense(10, activation='softmax'))       # 10 values in the output set => nb_classes
 model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['categorical_accuracy'])
+              loss='binary_crossentropy',
+              metrics=['binary_accuracy'])       # binary_accuracy : Calculates how often predictions match binary labels || vu qu'on encode les voyelles binary_accuracy est la bonne metric
 
-
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath="model/best_model_voyelles",
-    monitor='val_categorical_accuracy',
+    monitor='val_binary_accuracy',
     mode='max',
     verbose=1,
     save_best_only=True)
@@ -66,9 +75,9 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
 
 model.fit(x=x_train,
           y=y_train,
-          epochs=20,
+          epochs=100,
           validation_data=(x_test, y_test),
-          callbacks=[model_checkpoint_callback])
+          callbacks=[model_checkpoint_callback, tensorboard_callback])
 
 best_model = keras.models.load_model("model/best_model_voyelles")
 
